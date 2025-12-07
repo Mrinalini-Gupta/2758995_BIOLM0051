0aa2f223f921c6397e0629dbc666f54a852b9c22
Mrinalini Gupta <qo25519@bristol.ac.uk>
2025-12-07 15:27:06 +0000

    new blast result add and scripts run again

--- referenced_seq.sh
+++ referenced_seq.sh
@@ -1,6 +1,6 @@
 mkdir -p combined_seq
 echo "Combining sample and ref sequences"
-cat processed/sampleA.fasta sampleA_reference.fasta > combined_seq/sampleA_blast_seq.fasta
-cat processed/sampleB.fasta sampleB_reference.fasta > combined_seq/sampleB_blast_seq.fasta
-cat processed/sampleC.fasta sampleC_reference.fasta > combined_seq/sampleC_blast_seq.fasta
-cat processed/sampleD.fasta sampleD_reference.fasta > combined_seq/sampleD_blast_seq.fasta
+cat processed/sampleA.fasta sampleA_refs.fasta > combined_seq/sampleA_blast.fasta
+cat processed/sampleB.fasta sampleB_refs.fasta > combined_seq/sampleB_blast.fasta
+cat processed/sampleC.fasta sampleC_refs.fasta > combined_seq/sampleC_blast.fasta
+cat processed/sampleD.fasta sampleD_refs.fasta > combined_seq/sampleD_blast.fasta
