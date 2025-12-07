48a3e115c464b9296a4f02354c014ae29bd8bab5
Mrinalini Gupta <qo25519@bristol.ac.uk>
2025-12-07 11:52:52 +0000

    all samples and reference seq merged together

--- referenced_seq.sh
+++ referenced_seq.sh
@@ -1,6 +1,6 @@
 mkdir -p combined_seq
 echo "Combining sample and ref sequences"
-cat processed/sampleA.fasta sampleA_ref.fasta > combined_seq/sampleA_blast_seq.fasta
-cat processed/sampleB.fasta sampleB_ref.fasta > combined_seq/sampleB_blast_seq.fasta
-cat processed/sampleC.fasta sampleC_ref.fasta > combined_seq/sampleC_blast_seq.fasta
-cat processed/sampleD.fasta sampleD_ref.fasta > combined_seq/sampleD_blast_seq.fasta
+cat processed/sampleA.fasta sampleA_reference.fasta > combined_seq/sampleA_blast_seq.fasta
+cat processed/sampleB.fasta sampleB_reference.fasta > combined_seq/sampleB_blast_seq.fasta
+cat processed/sampleC.fasta sampleC_reference.fasta > combined_seq/sampleC_blast_seq.fasta
+cat processed/sampleD.fasta sampleD_reference.fasta > combined_seq/sampleD_blast_seq.fasta
